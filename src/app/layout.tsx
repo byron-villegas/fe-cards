56a54260b0d92f4f083986977eff76d6d334fbea
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-03-28 20:45:25 -0300

    Changed description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta charSet="utf-8" />
         <title>Cards</title>
-        <meta name="description" content="Application for my collection cards"></meta>
+        <meta name="description" content="An application to view and interact with my cards collection."></meta>
         <base href="/" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/webp" href="/favicon.webp" />
